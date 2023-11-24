a5ba8889a233845881fbacd19276d71e35a1e998
umutdrms <dumut938@gmail.com>
2023-11-24 12:49:03 +0300

    sql bağlantısı eklendi yeni masalar adında pencere açıldı

---
+++ Fonksiyonlar/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Staj_Projesi_Restoran_Yonetimi.Fonksiyonlar
+{
+     public class ConnectionString
+    {
+        // catalog, kullanıcı adı, şifre, şifreleme yönte
+        string connectionString = "Server=DESKTOP-MIOEHHS\\SQLEXPRESS;Database=Restoran;User Id=test_user;Password=test123;";
+
+
+    }
+}
